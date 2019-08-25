f6f3b87193906eec459593c2331b735464d6d0b5
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2019-08-25 14:55:05 -0400

    Mixed todo supervisor

---
+++ apps/todo/lib/todo/application.ex
@@ -0,0 +1,19 @@
+defmodule Todo.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Todo.Worker.start_link(arg)
+      # {Todo.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Todo.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
