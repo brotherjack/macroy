3771724cdc0b16dd3f0d0686e422b1e2618852ae
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2019-11-17 16:51:33 -0500

    Minor refactor

--- test/macroy/user_test.exs
+++ test/macroy/user_test.exs
@@ -1,28 +1,21 @@
 defmodule Macroy.UserTest do
   use Macroy.DataCase
 
-  @valid_users_params [
-      %{email: "dovakitty@xs4all.nl", password: "FusRoDah=>!"},
-      %{email: "wren@nick.com", password: "YouIDIOT!!!"}
-  ]
+  @dovakitty %{email: "dovakitty@xs4all.nl", password: "FusRoDah=>!"}
+  @wren %{email: "wren@nick.com", password: "YouIDIOT!!!"}
+  
 
   describe "insert_user/1" do
     test "can insert valid users" do
-      with dovakitty <- Enum.at(@valid_users_params, 0),
-           wren <- Enum.at(@valid_users_params, 1) do
-        assert {:ok, user1} = Macroy.insert_user(dovakitty)
-        assert user1.email == dovakitty.email
-        assert {:ok, user2} = Macroy.insert_user(wren)
-        assert user2.email == wren.email
-      end
+      assert {:ok, user1} = Macroy.insert_user(@dovakitty)
+      assert user1.email == @dovakitty.email
+      assert {:ok, user2} = Macroy.insert_user(@wren)
+      assert user2.email == @wren.email
     end
 
     test "rejects user changesets without valid fields" do
-      with dovakitty <- Enum.at(@valid_users_params, 0),
-           wren <- Enum.at(@valid_users_params, 1) do
-        assert {:error, _} = Macroy.insert_user(Map.take(dovakitty, [:email]))
-        assert {:error, _} = Macroy.insert_user(Map.take(wren, [:email]))
-      end
+      assert {:error, _} = Macroy.insert_user(Map.take(@dovakitty, [:email]))
+      assert {:error, _} = Macroy.insert_user(Map.take(@wren, [:email]))
     end
   end
 end
