c2243a8b1d913e9c6a24c7fa15fb0584f2eb373d
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2019-11-26 18:02:19 -0500

    Added migration to give Todo an owner

---
+++ priv/repo/migrations/20191126225748_todos_are_now_owned_by_users.exs
@@ -0,0 +1,15 @@
+defmodule Macroy.Repo.Migrations.TodosAreNowOwnedByUsers do
+  use Ecto.Migration
+
+  def up do
+    alter table("todos") do
+      add :owner_id, references(:users, on_delete: :delete_all), null: false
+    end
+  end
+
+  def down do
+    alter table("todos") do
+      remove :owner_id
+    end
+  end
+end
