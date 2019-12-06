03b679a357d98a59c89015ddca8551b2d76b159a
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2019-12-05 19:49:02 -0500

    Added new/2, create/2, and show/2 functions

--- lib/macroy_web/controllers/todo_controller.ex
+++ lib/macroy_web/controllers/todo_controller.ex
@@ -1,11 +1,33 @@
 defmodule MacroyWeb.TodoController do
   use MacroyWeb, :controller
   import Phoenix.LiveView.Controller
+  alias Macroy.Todo
 
   def index(conn, _params) do
     todos = conn.assigns.current_user.id
     |> Macroy.list_todos()
-
+    
     live_render(conn, MacroyWeb.TodoLive, session: %{todos: todos})
+  end
+
+  def new(conn, _params) do
+    todo_f = Todo.get_todo_fields_with_types()
+    render(conn, "new.html", todo: Macroy.new_todo(), todo_fields: todo_f)
+  end
+
+  def create(conn, %{"todo" => todo_params}) do
+    inserted_todo = todo_params
+    |> Map.put("owner_id", conn.assigns.current_user.id)
+    |> Macroy.insert_todo()
+
+    case inserted_todo do
+      {:ok, todo} -> redirect(conn, to: Routes.todo_path(conn, :show, todo))
+      {:error, todo} -> render(conn, "new.html", todo: todo)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    todo = Macroy.get_todo(id)
+    render(conn, "show.html", todo: todo)
   end
 end
